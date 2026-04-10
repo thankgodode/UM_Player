1c72ac320753e7104cb1a6548dcfdd2f61888f68
Thankgod Ode <thankgodode535@gmail.com>
2026-04-11 00:14:54 +0100

    d

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { BackHandler, ScrollView, StyleSheet, View } from "react-native";
-import ActionBar from "../../components/ui/ActionBar";
+import Navbar from "../../components/ui/Navbar";
 import VideoScreen from "../../components/ui/RecentWatched";
 import SlideMenu from "../../components/ui/SlideMenu";
 import VideoFolders from "../../components/ui/TabVideoFolder";
@@ -19,8 +19,8 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      {/* <Navbar title="Video" /> */}
-      <ActionBar count={100} />
+      <Navbar title="Video" />
+      {/* <ActionBar count={100} /> */}
       <SlideMenu />
       <ScrollView
         horizontal={true}
